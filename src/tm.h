ae404cafbf7b4480662ddb7f27271e955d771f7b
Jorge Miguel Ferreira da Silva <jorgeMFS@users.noreply.github.com>
2019-02-18 18:21:04 +0000

    Update .h to resolve conflits

--- src/tm.h
+++ src/tm.h
@@ -1,5 +1,13 @@
 #include <vector>
 
+
+struct CompressionResultsData
+{
+  std::vector <unsigned int> amplitudeResults;
+  std::vector <double> normalizedCompressionResults;
+  std::vector <double> selfCompressionResults;
+};
+
 /** Evaluate all relative turing machine programs with the given architecture.
  *
  * @param states
@@ -9,7 +17,7 @@
  * @param verbose
  * @return a vector of normalized compression values, one per turing machine
  */
-std::vector<double> tm(
+CompressionResultsData tm(
     size_t states,
     size_t alphabet_size,
     unsigned int num_iterations,
