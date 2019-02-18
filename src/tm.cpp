2620fbd16759268864c652515dbaac7748417592
Jorge Miguel Ferreira da Silva <jorgeMFS@users.noreply.github.com>
2019-02-18 18:22:54 +0000

    Update tm.cpp to resolve conflits

--- src/tm.cpp
+++ src/tm.cpp
@@ -15,15 +15,15 @@
  * @param verbose
  * @return a vector of normalized compression values, one per turing machine
  */
-std::vector<double> tm(
+CompressionResultsData tm(
     size_t states,
     size_t alphabet_size,
     unsigned int num_iterations,
     unsigned int k,
     bool verbose) {
+
   TuringMachine machine(states, alphabet_size);
-  std::vector <double> avg_nc_results;
-  bool normalizer = true;
+  CompressionResultsData data;
   NormalizedCompressionMarkovTable normalizedCompressionMarkovTable(k , alphabet_size);
 
   unsigned int counter = 0;  
@@ -33,17 +33,22 @@ std::vector<double> tm(
       normalizedCompressionMarkovTable.mrkvTable.reset();
       machine.act(); //importante ser antes
     }
-    double normalizedCompressionValue = normalizedCompressionMarkovTable.update_nc_mk_table(machine.turingTape, normalizer);
-    avg_nc_results.push_back(normalizedCompressionValue);
+    Metrics normalizedCompressionValue = normalizedCompressionMarkovTable.update_nc_mk_table(machine.turingTape);
+    data.amplitudeResults.push_back(normalizedCompressionValue.amplitude);
+    data.selfCompressionResults.push_back(normalizedCompressionValue.selfCompression);
+    data.normalizedCompressionResults.push_back(normalizedCompressionValue.normalizedCompression);
 
     if (verbose && counter % 65536 == 0) {
-      std::cerr << "TM #" << std::setw(8) << counter << ": ncv = " << normalizedCompressionValue << "\r";
+      std::cerr << "TM #" << std::setw(8) << counter << ": amplitude = " << normalizedCompressionValue.amplitude 
+      << ": sc = " << std::setprecision(5) <<normalizedCompressionValue.selfCompression <<": nc = " << std::setprecision(5) 
+      << normalizedCompressionValue.normalizedCompression << "\r";
     }
     counter += 1;
     normalizedCompressionMarkovTable.mrkvTable.reset();
   } while (machine.stMatrix.next());
+  
   if (verbose) {
     std::cerr << std::endl;
   }
-  return avg_nc_results;
+  return data;
 }
