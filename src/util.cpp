c4e084210b38a746dc772debedd51b8452e752a1
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2019-02-25 15:16:56 +0000

    fixed problem of cardinallity

--- src/util.cpp
+++ src/util.cpp
@@ -23,6 +23,52 @@ unsigned long long ipow(unsigned long long base, unsigned long long exp) {
     return result;
 }
 
+#define MAX 100000 
+
+int multiply(int x, int res[], int res_size) { 
+  int carry = 0; 
+  for (int i = 0; i < res_size; i++) { 
+      int prod = res[i] * x + carry; 
+      res[i] = prod % 10; 
+      carry = prod / 10; 
+  } 
+  
+  while (carry) { 
+      res[res_size] = carry % 10; 
+      carry = carry / 10; 
+      res_size++; 
+  } 
+return res_size; 
+} 
+  
+// This function finds power of a number x 
+void power(int x, int n)  
+{
+  if(n == 0 ){  
+      std::cout<<"1"; 
+      return; 
+  }  
+  int res[MAX]; 
+  int res_size = 0; 
+  int temp = x; 
+  
+  while (temp != 0) { 
+      res[res_size++] = temp % 10; 
+      temp = temp / 10; 
+  } 
+    
+  for (int i = 2; i <= n; i++) 
+      res_size = multiply(x, res, res_size); 
+
+  for (int i = res_size - 1; i >= 0; i--) 
+      std::cout << res[i]; 
+} 
+
+
+
+/////
+
+
 /// Calculate the full cardinality of all possible turing machine state matrices
   unsigned long long tm_cardinality(size_t states, size_t alphabet_size) {
     
@@ -32,6 +78,16 @@ unsigned long long ipow(unsigned long long base, unsigned long long exp) {
 
     return ipow(record_cardinality, nstates * nalphabet);
 }
+
+/// Calculate the full cardinality of all possible turing machine state matrices
+  void tm_fl_cardinality(size_t states, size_t alphabet_size) {
+    
+    int nstates = states;
+    int  nalphabet = alphabet_size;
+    auto record_cardinality = nstates * nalphabet * 3;
+    power(record_cardinality, nstates * nalphabet);
+}
+
 /// Create the list of the number of machines with the growth of the number of states.
   void tm_growth_with_cardinality(size_t max_number_states){
     size_t min_number_states = 1;
@@ -41,7 +97,9 @@ unsigned long long ipow(unsigned long long base, unsigned long long exp) {
      std::cout << "state" << "\t" <<"Number of Turing Machines"<< std::endl;
 
     for(auto state = min_number_states; state != max_number_states; ++state){
-        std::cout << state << "\t" << tm_cardinality(state, alphabet_size) << std::endl;
+        std::cout << state << "\t"; 
+        tm_fl_cardinality(state, alphabet_size);
+        std::cout << std::endl;
     }
   }
   
--- src/util.h
+++ src/util.h
@@ -22,3 +22,15 @@ unsigned long long tm_cardinality(size_t states, size_t alphabet_size);
  * @param max_number_states the number of states cardinality
  */
 void tm_growth_with_cardinality(size_t max_number_states);
+
+
+void tm_fl_cardinality(size_t states, size_t alphabet_size);
+
+
+// This function finds power of a number x 
+void power(int x, int n);
+
+/* This function multiplies x  with the number represented by res[]. 
+ res_size is size of res[] or number of digits in the number represented by res[]. This function uses simple school mathematics for multiplication. 
+This function may value of res_size and returns the new value of res_size */
+int multiply(int x, int res[], int res_size);
