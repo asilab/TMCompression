43035a0d2427ba41f9378bbbe47629f902204cad
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2019-04-05 11:44:43 +0100

    added mechanism to recreate article results

--- scripts/downloadResults.sh
+++ scripts/downloadResults.sh
@@ -6,9 +6,9 @@
 
 
 function gdrive_download () {
-  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2 -P ../resultText
-  rm -rf /tmp/cookies.txt
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p');
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2 -P ../resultText;
+  rm -rf /tmp/cookies.txt;
 }
 
 gdrive_download 1AEp4Zw5kvlBF_52-K_qTyYkzHAIPYBad 2sts2alp.txt;
